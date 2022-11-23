ef5bc13556557512478dac20b0f474e196490687
Martin Kiperszmid <MKiperszmid@gmail.com>
2022-11-22 21:54:32 -0300

    Extracted Strings to Resources

--- app/src/main/java/com/mk/moviedb/home/presentation/HomeScreen.kt
+++ app/src/main/java/com/mk/moviedb/home/presentation/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mk.moviedb.R
@@ -26,7 +27,7 @@ fun HomeScreen(
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.logo_marca),
-                    contentDescription = "eMovie"
+                    contentDescription = stringResource(R.string.emovie)
                 )
                 // TODO: The Logo from FIGMA couldn't be exported.
             }
@@ -35,7 +36,7 @@ fun HomeScreen(
         if (state.upcoming.isNotEmpty()) {
             item {
                 HomeMovieList(
-                    "Proximos Estrenos",
+                    stringResource(R.string.upcoming_releases),
                     posters = state.upcoming.map { it.poster }
                 )
             }
