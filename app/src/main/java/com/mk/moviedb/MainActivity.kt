6063adff0a9604f42dafd4c0b38932e4ac111190
Martin Kiperszmid <MKiperszmid@gmail.com>
2022-11-22 22:56:41 -0300

    Splash Screen

--- app/src/main/java/com/mk/moviedb/MainActivity.kt
+++ app/src/main/java/com/mk/moviedb/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -18,6 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MovieDbTheme {
                 // A surface container using the 'background' color from the theme
